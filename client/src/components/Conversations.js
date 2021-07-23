2d0c07551167360245b0487ad3e0578babe47de9
Sam Lalli <2sam.lalli@gmail.com>
2021-07-23 16:46:30 -0600

    sidebar complete conversations and contacts added, ability to add and view completed held in localstorage

---
+++ client/src/components/Conversations.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ListGroup } from 'react-bootstrap'
+import { useConversations } from '../contexts/ConversationsProvider'
+
+export default function Conversations() {
+    const { conversations } = useConversations()
+
+    return (
+        <ListGroup variant="flush">
+            {conversations.map((conversation, index) => (
+                <ListGroup.Item key={index}>
+                    {conversation.recipients.map(r => r.name).join(', ')}
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    )
+}
